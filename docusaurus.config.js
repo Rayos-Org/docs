ae43560ccf0a1fbb40738f560966e08a82ed75b2
thisisouvik <souvikmandals10@gmail.com>
2026-09-12 18:44:00 +0530

    Add initial Docusaurus configuration for Rayos documentation site

---
+++ docusaurus.config.js
@@ -0,0 +1,110 @@
+// @ts-check
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Rayos Documentation',
+  tagline: 'Seedless, passkey-native smart wallets for Stellar',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://docs.rayos.org',
+  baseUrl: '/',
+
+  organizationName: 'Rayos-Org',
+  projectName: 'docs',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/', // Serve docs at the root
+          sidebarPath: './sidebars.js',
+          editUrl: 'https://github.com/Rayos-Org/docs/tree/main/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Rayos',
+        logo: {
+          alt: 'Rayos Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Documentation',
+          },
+          {
+            href: 'https://github.com/Rayos-Org',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Introduction',
+                to: '/',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/rayos',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/rayos',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/Rayos-Org',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Rayos Ecosystem. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
